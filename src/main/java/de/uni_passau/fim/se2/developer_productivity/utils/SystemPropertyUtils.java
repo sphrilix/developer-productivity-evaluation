22adacae334fde62b2626faee93d5cb039f76fd5
sphrilix <maxi.jungwirth@googlemail.com>
2022-12-11 22:54:44 +0100

    fix compilation error

---
+++ src/main/java/de/uni_passau/fim/se2/developer_productivity/utils/SystemPropertyUtils.java
@@ -0,0 +1,8 @@
+package de.uni_passau.fim.se2.developer_productivity.utils;
+
+public class SystemPropertyUtils {
+
+    public static String resolvePlaceholders(String s) {
+        return "";
+    }
+}
